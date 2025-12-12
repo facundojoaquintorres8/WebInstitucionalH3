248a99872e599422299f4352d3c8323c32141fb7
Facundo Torres <ftorres@ubicuagroup.com>
2025-12-12 20:16:08 -0300

    nace una nueva web institucional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'products', component: Products},
+  { path: 'contact', component: Contact },
+];
